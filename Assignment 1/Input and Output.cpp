777c7abce14f4bd229ba3b200881df1b4fafe15d
Sparkout <116503540+adibr01@users.noreply.github.com>
2023-04-30 23:06:32 +0530

    Update Input and Output.cpp

--- Assignment 1/Input and Output.cpp
+++ Assignment 1/Input and Output.cpp
@@ -7,6 +7,10 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+     int a,b,c,x;
+    scanf("%d%d%d",&a,&b,&c);
+    x=a+b+c;
+    printf("%d",x);
     return 0;
 }
